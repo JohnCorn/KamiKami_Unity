e1d47425971423c4f5d35dbe9b4d76b3ef2c7d42
JohnCorn <johncornacchioli@Johns-iMac.local>
2014-12-11 15:21:16 -0800

    Entire game so far

---
+++ Kamikami/Assets/Scripts/Jump.js
@@ -0,0 +1,27 @@
+﻿var canJump : boolean = true;
+var Jump = 100.0;
+
+function Update ()
+{
+if(Input.GetKeyDown("space") && canJump)
+{
+this.rigidbody2D.AddForce(Vector3.up * Jump * Time.deltaTime);
+//this.rigidbody2D.velocity.y += Jump; //jump
+canJump = false; //Disable jumping until landing
+print ("Jump");
+
+}
+else if (Input.GetKeyDown (KeyCode.JoystickButton1) && canJump)
+{
+this.rigidbody2D.velocity.y += 3.75; //jump
+canJump = false; //Disable jumping until landing
+print ("Jump");
+
+}
+}
+function OnCollisionEnter2D(other : Collision2D){
+
+if(other.transform.tag == "Ground"){ //If the player lands on ground
+    canJump = true; //allow him to jump again
+}
+}
---
+++ Kamikami/Assets/Scripts/Move_PC.js
@@ -0,0 +1,30 @@
+﻿var runSpeed = 10.0;
+
+ 
+
+function Update(){
+
+ 
+
+    //Movement buttons are pressed
+
+    if(Input.GetAxis("Horizontal")){
+
+    
+
+        //Set some variables for movement
+
+        var moveSpeed : float = Input.GetAxis("Horizontal")*runSpeed *.25;
+
+ 
+
+        //Move our character
+
+        transform.Translate(moveSpeed, 0, 0);
+
+
+    }
+
+ 
+
+}
